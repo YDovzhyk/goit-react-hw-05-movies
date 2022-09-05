28e9a9844f703fb8f4d5e9e4f18493ec7c32ac7d
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2022-09-05 14:38:22 +0300

    add 404 page
    add 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,8 @@
-
+import s from "../components/App.module.css"
 const NotFoundPage = () => {
     return (
-        <div className="container">
-            <h1 className="page-title">Page not found</h1>
+        <div className={s.container}>
+            <img src={`https://img.freepik.com/free-vector/404-error-with-a-landscape-concept-illustration_114360-7888.jpg?w=2000`} alt="Page not found" className={s.image} />
         </div>
     )
 }
