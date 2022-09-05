d83719dc3b8e6b5fd1aa72146ed93d58e6f4d72d
YDovzhyk <48929257+YDovzhyk@users.noreply.github.com>
2022-09-05 05:52:10 +0000

    Deploying to gh-pages from @ YDovzhyk/goit-react-hw-05-movies@27ae70c4a15bc04df9a9527860a17f5fae9121ca 🚀

---
+++ static/js/520.f29a4e49.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{4520:function(e,t,a){a.r(t);var n=a(501),r=a(184);t.default=function(){return(0,r.jsxs)("div",{className:"container",children:[(0,r.jsx)("h1",{className:"page-title",children:"Page not found"}),(0,r.jsx)(n.rU,{to:"/",children:"To home page"})]})}}}]);
+//# sourceMappingURL=520.f29a4e49.chunk.js.map
