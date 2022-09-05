be5f49b52cf0f2094f2779d1d55f9cdece847bc0
YDovzhyk <48929257+YDovzhyk@users.noreply.github.com>
2022-09-05 11:39:17 +0000

    Deploying to gh-pages from @ YDovzhyk/goit-react-hw-05-movies@28e9a9844f703fb8f4d5e9e4f18493ec7c32ac7d 🚀

---
+++ static/js/520.7c6dd3f0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{4520:function(e,t,a){a.r(t);var r=a(1289),c=a(184);t.default=function(){return(0,c.jsx)("div",{className:r.Z.container,children:(0,c.jsx)("img",{src:"https://img.freepik.com/free-vector/404-error-with-a-landscape-concept-illustration_114360-7888.jpg?w=2000",alt:"Page not found",className:r.Z.image})})}}}]);
+//# sourceMappingURL=520.7c6dd3f0.chunk.js.map
